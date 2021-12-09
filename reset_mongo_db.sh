fdf261bf4a8faef6f8c73d6ccda0b04bf2c8b3c4
Alex <alexdemulder@gmail.com>
2021-12-09 13:33:00 +0100

    combined scripts

--- reset_mongo_db.sh
+++ reset_mongo_db.sh
@@ -1,3 +1,19 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-mongo localhost:27017 ${SCRIPT_DIR}/scripts/mongo.js
+JS_STRING="
+db_files = connect(\"files_v1\")
+db_users = connect(\"users_v1\")
+db_data  = connect(\"data_v1\")
+
+db_files.dropDatabase()
+db_users.dropDatabase()
+db_data.dropDatabase()
+"
+
+# create tmp js file to instruct mongo
+TMP_FILE_PATH=${SCRIPT_DIR}/tmp_mongo_instructions.js
+rm ${TMP_FILE_PATH}
+echo ${JS_STRING} >> ${TMP_FILE_PATH}
+
+mongo localhost:27017 ${TMP_FILE_PATH}
+rm ${TMP_FILE_PATH}
